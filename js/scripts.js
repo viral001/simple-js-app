2f46c4511890d5f209a1c80e3ab99dc5f76d5ceb
viral001 <57367664+viral001@users.noreply.github.com>
2019-11-15 02:07:10 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+alert('Hello world');
+
+var favoriteFood='pizza';
+document.write(favoriteFood);
+
+var myName='Viral';
+document.write(myName);
+
+var size = 100;
+var doubleSize = size * 2;
+var minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
