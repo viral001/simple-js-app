20c24d57f5b8ef33f5cf65d77af099a4f4f8a3b9
viral001 <viral_1996@yahoo.com>
2019-11-16 11:11:38 -0500

    pokemon
    added 4 new character. added name, height, weights and types.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var repository = [ /* repository of pokemon ];
+  {
+    name: 'Wartortle';
+    height: '1m';
+    weight: '22.5kg';
+    type: 'water';
+  }
+  {
+    name: 'Blastoise';
+    height: '1.6m';
+    weight: '85.5';
+    type: 'water';
+  }
+  {
+    name: 'Bulbasaur';
+    height: '0.7m';
+    weight: '6.9kg';
+    type: 'grass , poison';
+  }
+  {
+    name: 'Charizard';
+    height: '1.7m';
+    weight: '90.5kg';
+    type: 'fire, flying';
+  }
