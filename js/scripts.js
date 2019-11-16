babf6c7a6c8a62d80f5ae9a97c90eb07c8323b26
viral001 <viral_1996@yahoo.com>
2019-11-16 11:27:56 -0500

    hyphen
    hyphen removed removed from end of the names and added comma.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 var repository = [ /* repository of pokemon*/
   {
-    name: 'Wartortle';
-    height: '1m';
-    weight: '22.5kg';
-    type: 'water';
-  }
+    name: 'Wartortle',
+    height: 1,
+    weight: 22.5,
+    type: ['water']
+  },
   {
-    name: 'Blastoise';
-    height: '1.6m';
-    weight: '85.5';
-    type: 'water';
-  }
+    name: 'Blastoise',
+    height: 1.6,
+    weight: 85.5,
+    type: ['water']
+  },
   {
-    name: 'Bulbasaur';
-    height: '0.7m';
-    weight: '6.9kg';
-    type: 'grass , poison';
-  }
+    name: 'Bulbasaur',
+    height: 0.7,
+    weight: 6.90,
+    type: ['grass , poison']
+  },
   {
-    name: 'Charizard';
-    height: '1.7m';
-    weight: '90.5kg';
-    type: 'fire, flying';
+    name: 'Charizard',
+    height: 1.7,
+    weight: 90.5,
+    type: ['fire, flying']
   }
 ]
