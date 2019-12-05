9389c7200127737ccd3f19e78d5a0618765b0b6c
viral001 <viral_1996@yahoo.com>
2019-12-05 12:14:52 -0500

    Revert "Update scripts.js"
    This reverts commit 59bd3f9ac8552eb8f388c2af9a2ba40ce90fac03.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,5 @@
 var pokemonRepository = (function () {
   var repository = [
-    var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-    // Other functions remain here
-
-  function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        var pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
-  }
-
-  return {
-    add: add,
-    getAll: getAll,
-    search: search,
-    loadList: loadList
-  };
-})();
-
-pokemonRepository.loadList().then(function() {
-  // Now the data is loaded!
-  pokemonRepository.getAll().forEach(function(pokemon){
-    addListItem(pokemon);
-  });
-});
 
     {name:"Bulbasaur",
     height: 7,
