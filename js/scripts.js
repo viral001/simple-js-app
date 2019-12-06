85751d01a850e300f3a05aa202b15d654aff3360
viral001 <viral_1996@yahoo.com>
2019-12-06 12:33:58 -0500

    modal
    added modal to JS file

--- js/scripts.js
+++ js/scripts.js
@@ -66,6 +66,79 @@ function loadDetails(item) {
 });
 }
 
+// creating model content
+function showModel(item) {
+  var $modalcontainer = document.querySelector('#modal-container');
+
+// clearing all existing modal content
+$modalcontainer.innerHTML = '';
+
+// create div element in DOM
+var modal = document.createElement('div');
+// Add class to div DOM element
+modal.classList.add('modal');
+
+// creating element for name in modal content
+var nameElement = document.createElement('h1');
+nameElement.innerText = item.name;
+
+// creating img in modal content
+var imageElement = document.createElement('img');
+  imageElement.classList.add('modal-img');
+  imageElement.setAttribute('src', item.imageUrl);
+
+// Creating element for height
+var heightElement = document.createElement('p');
+  heightElement.innerText = 'height : ' + item.height;
+
+// Creating element for weight in modal content
+var typesElement = document.createElement('p');
+  typesElement.innerText = 'weight : ' + item.weight;
+
+// Create closing button in modal content
+var closeButtonElement = document.createElement('button');
+  closeButtonElement.classList.add('modal-close');
+  closeButtonElement.innerText = 'Close';
+  closeButtonElement.addEventListener('click', hideModal);
+
+// Appending modal content to webpage
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(nameElement);
+  modal.appendChild(imageElement);
+  modal.appendChild(heightElement);
+  modal.appendChild(typesElement);
+  $modalContainer.appendChild(modal);
+
+// adds class to show the modal
+  $modalContainer.classList.add('is-visible');
+}
+
+// hides modal when you click on close button
+function hideModal() {
+  var $modalContainer = document.querySelector('#modal-container');
+    $modalContainer.classList.remove('is-visible');
+}
+
+// Hides modal when clicked on ESC on keyboard
+window.addEventListener('keydown', (e) => {
+  var $modalContainer = document.querySelector('#modal-container');
+if (
+  e.key === 'Escape' && $modalContainer.classList.contains('is-visible')
+) {
+  hideModal();
+}
+});
+
+// Hides modal if clicked outside of it
+var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.addEventListener('click', (e) => {
+var target = e.target;
+  if (target === $modalContainer) {
+    hideModal();
+}
+});
+}
+
 return {
   loadList: loadList,
   addListItem: addListItem,
@@ -77,12 +150,10 @@ return {
 
 })();  //this one closes IIFE
 
-// console.log(pokemonRepository.getAll());
-// console.log(pokemonRepository.getAll());
 
 pokemonRepository.loadList().then(function() {
 //loop
-  pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.getAll().forEach(function(pokemon){   //loop
     pokemonRepository.addListItem(pokemon);
   });
 });
